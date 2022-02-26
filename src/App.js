5b34db9a1374f1f8d7c8e3b1aa7503ee6cf9c9e5
Supratim Dey <supratimdey@outlook.com>
2022-02-26 17:59:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  console.log("Hello");
+  return (
+    <div className="App">
+      <h1>Hello There</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
